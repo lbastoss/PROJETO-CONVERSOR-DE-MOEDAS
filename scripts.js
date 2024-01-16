eaf04eb4ee05f62241df90ea6fc6bc747a336b34
lbastos17 <bastosls17@gmail.com>
2024-01-16 18:59:05 -0300

    adicinando js no projeto

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const convertButton= document.querySelector(".convert-button")
+
+function convertValues () {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+
+    const dolarToday = 4.93
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    console.log (convertedValue)
+
+}
+convertButton.addEventListener("click", convertValues )
+
+
+
+
+
