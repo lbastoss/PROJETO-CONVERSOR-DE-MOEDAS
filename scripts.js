252c6abb5d413fbcc595afa9c8bb8bf9a9c65765
lbastos17 <bastosls17@gmail.com>
2024-01-17 16:30:31 -0300

    atualizando js, botões euro e dolar

--- scripts.js
+++ scripts.js
@@ -1,27 +1,50 @@
-const convertButton= document.querySelector(".convert-button")
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-function convertValues () {
+
+
+function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currecyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em real
     const currencyValueConverted = document.querySelector(".currency-value") // outras moedas
+
     
     const dolarToday = 4.93
     const EuroToday = 5.36
 
-    const convertedValue = inputCurrencyValue / dolarToday
 
-    currecyValueToConvert.innerHTML = new Intl.NumberFormat ("pt-BR", {
+
+    if (currencySelect.value == "dolar") {
+        // se o select estiver o valor de dolar, entre aqui 
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+
+    }
+
+
+    if (currencySelect.value == "euro") {
+        // se o select estiver o valor de euro, entre aqui 
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / EuroToday)
+    }
+
+
+
+    currecyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
-    currencyValueConverted.innerHTML = new Intl.NumberFormat ("en-US", {
-        style: "currency", currency: "USD" 
-    }).format(convertedValue)
-   
-    console.log (convertedValue)
+
+
+    console.log(convertedValue)
 
 }
-convertButton.addEventListener("click", convertValues )
+convertButton.addEventListener("click", convertValues)
 
 
 
